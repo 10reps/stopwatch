d0a4165e12f22eb618b392d654975703fbbe16a9
Seohong Kim <greyxcx@gmail.com>
2022-12-27 21:55:11 +0900

    Create reset button function

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const btnBox = document.querySelector('.button-box');
 const playBtn = document.querySelector('.play-btn');
 const stopBtn = document.querySelector('.stop-btn');
+const resetBtn = document.querySelector('.reset-btn');
 const time = { elapsedTime: 0 };
 const minute = document.querySelector('.minute');
 const second = document.querySelector('.second');
@@ -12,16 +13,23 @@ playBtn.addEventListener('click', () => {
         start();
     } else {
         time.elapsedTime += Date.now() - time.startTime;
-        clearInterval(time.intervalId);
+        clearInterval(time.interval);
         playBtn.classList.replace('stop-btn', 'play-btn');
         playBtn.innerHTML = '<i class="ic-play"></i>START';
     };
 });
 
+resetBtn.addEventListener('click', () => {
+    time.elapsedTime = 0;
+    time.startTime = Date.now();
+    secondTime(0);
+    minuteTime(0);
+});
+
 function start() {
     time.startTime = Date.now();
 
-    time.intervalId = setInterval(() => {
+    time.interval = setInterval(() => {
         const elapsedTime= Date.now() - time.startTime + time.elapsedTime;
         const minutes = parseInt((elapsedTime/(1000*60))%60);
         const seconds = parseInt((elapsedTime/1000)%60);
